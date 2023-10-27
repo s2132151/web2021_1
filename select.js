e5ba9009a29d8b2e04b537ffd75973c18408ad7b
s2132151 <149139493+s2132151@users.noreply.github.com>
2023-10-27 08:11:58 +0000

    1027

--- select.js
+++ select.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test2.db');
 
 let sql = `
-select id, name from test;
+select id, name from car;
 `
 
 db.serialize( () => {
